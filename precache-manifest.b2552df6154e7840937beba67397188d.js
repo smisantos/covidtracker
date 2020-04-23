c61d2f3e5e1497a85252a7d02d270c341a811f6a
Sandro Santos <smisantos@gmail.com>
2020-04-23 22:33:03 +0100

    Updates

---
+++ precache-manifest.b2552df6154e7840937beba67397188d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce9c47a61f4e7cff86eb8acdea7a4c8",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "7b17d4bd790b162af538",
+    "url": "/covidtracker/static/css/main.80c96ef6.chunk.css"
+  },
+  {
+    "revision": "a1fb2f87cfac850cc36f",
+    "url": "/covidtracker/static/js/2.5d654655.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.5d654655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b17d4bd790b162af538",
+    "url": "/covidtracker/static/js/main.9f99e0f0.chunk.js"
+  },
+  {
+    "revision": "d90de5bd96b7581cb19e",
+    "url": "/covidtracker/static/js/runtime-main.0cd38f76.js"
+  },
+  {
+    "revision": "6ae527896a30410a5b5a504d51edccda",
+    "url": "/covidtracker/static/media/logo.6ae52789.png"
+  }
+]);
